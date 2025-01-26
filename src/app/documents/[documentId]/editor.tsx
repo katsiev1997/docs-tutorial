e7e3172f8e0b984f0c21c19933bf2d91ba7766be
katsiev1997 <katsiev1997@gmail.com>
2025-01-26 23:21:45 +0300

    edtior tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -10,6 +10,7 @@ import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
 import Image from "@tiptap/extension-image";
 import ImageResize from "tiptap-extension-resize-image";
+import Underline from "@tiptap/extension-underline";
 
 import { useEditorStore } from "@/store/use-editor-store";
 
@@ -57,6 +58,7 @@ export const Editor = () => {
 			TableHeader,
 			Image,
 			ImageResize,
+			Underline,
 		],
 		content: `
         <table>
